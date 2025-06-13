1f756932e8e5754c6d7d1d9bd87fc9de43331252
nubis <nubis@constata.eu>
2025-06-13 18:39:04 +0200

    Adds tracking of pool size and token holdings to payouts

--- api/src/models/holder.rs
+++ api/src/models/holder.rs
@@ -34,6 +34,8 @@ model! {
     #[sqlx_model_hints(varchar)]
     amount: String,
     #[sqlx_model_hints(varchar)]
+    balance: String,
+    #[sqlx_model_hints(varchar)]
     contract_cycle: String,
     #[sqlx_model_hints(timestamptz, default)]
     created_at: DateTime<Utc>,
@@ -41,7 +43,7 @@ model! {
 }
 
 impl EstimatedFeePoolClaimHub {
-    pub async fn create_if_missing(&self, holder: &Holder, amount: U256, contract_cycle: U256) -> AsamiResult<()> {
+    pub async fn create_if_missing(&self, holder: &Holder, amount: U256, balance: U256, contract_cycle: U256) -> AsamiResult<()> {
         if self
             .select()
             .holder_id_eq(holder.id())
@@ -57,6 +59,86 @@ impl EstimatedFeePoolClaimHub {
             holder_id: *holder.id(),
             amount: amount.encode_hex(),
             contract_cycle: contract_cycle.encode_hex(),
+            balance: balance.encode_hex(),
+        })
+        .save()
+        .await?;
+
+        Ok(())
+    }
+
+    pub async fn populate_new_column_data(&self) -> AsamiResult<()> {
+        let snapshots = [
+            (
+                "0x0000000000000000000000000000000000000000000000000000000000000019".to_string(),
+                u("2800000")
+            ),
+            (
+                "0x000000000000000000000000000000000000000000000000000000000000001a".to_string(),
+                u("2900000")
+            ),
+        ];
+
+        for (cycle, supply) in snapshots {
+            let mut pool = u("0");
+            for claim in self.state
+                .estimated_fee_pool_claim()
+                .select()
+                .contract_cycle_eq(&cycle)
+                .all().await? {
+                    pool += u256(claim.amount());
+                }
+
+            self.state.fee_pool_snapshot().create_if_missing(u256(&cycle), pool, supply).await?;
+            let claims = self.state.estimated_fee_pool_claim().select()
+                .balance_eq(weihex("0"))
+                .contract_cycle_eq(&cycle)
+                .all()
+                .await?;
+
+            dbg!(&claims);
+
+            for claim in claims {
+                let balance = u256(claim.amount()) * supply / pool;
+                claim.update().balance(balance.encode_hex()).save().await?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+model! {
+  state: App,
+  table: fee_pool_snapshots,
+  struct FeePoolSnapshot {
+    #[sqlx_model_hints(int4, default)]
+    id: i32,
+    #[sqlx_model_hints(varchar)]
+    cycle: String,
+    #[sqlx_model_hints(varchar)]
+    pool: String,
+    #[sqlx_model_hints(varchar)]
+    supply: String,
+  }
+}
+
+impl FeePoolSnapshotHub {
+    pub async fn create_if_missing(&self, cycle: U256, pool: U256, supply: U256) -> AsamiResult<()> {
+        if self
+            .select()
+            .cycle_eq(cycle.encode_hex())
+            .count()
+            .await?
+            > 0
+        {
+            return Ok(());
+        }
+
+        self.insert(InsertFeePoolSnapshot {
+            cycle: cycle.encode_hex(),
+            pool: pool.encode_hex(),
+            supply: supply.encode_hex(),
         })
         .save()
         .await?;
