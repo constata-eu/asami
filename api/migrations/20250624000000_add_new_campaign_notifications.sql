456f866eb8c04b2667d71648084eb606509e7edd
nubis <nubis@constata.eu>
2025-06-25 14:07:49 +0200

    skip existing campaigns from announcements

--- api/migrations/20250624000000_add_new_campaign_notifications.sql
+++ api/migrations/20250624000000_add_new_campaign_notifications.sql
@@ -1,1 +1,2 @@
 ALTER TABLE campaigns ADD COLUMN x_announcement_id_en VARCHAR, ADD COLUMN x_announcement_id_es VARCHAR;
+UPDATE campaigns set x_announcement_id_en = 'skipped', x_announcement_id_es = 'skipped';
